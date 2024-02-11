4a0901085d3250fa3834a8ece6af2c83afd1e947
Sushgiri <girisushil1002@gmail.com>
2024-02-11 10:12:50 +0530

    jenkins test 1

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+     System.out.println("helloo world ");System.out.println(" i am first test of jenkins");System.out.println("helloo world ");
+        System.out.println(" how r uuuuuu ");
+        }
+
+}
